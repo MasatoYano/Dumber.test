b563826b3acd3b6e1991627cdb88afd3e6a01692
Masato Yano <yanomasato0315@icloud.com>
2024-06-18 23:11:54 +0900

    Initial Commit

---
+++ Dumber/DumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  DumberApp.swift
+//  Dumber
+//
+//  Created by 矢野雅士 on 2024/06/18.
+//
+
+import SwiftUI
+
+@main
+struct DumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
